4fa3b8631afbcc295143affcd30cb6f364e9a911
unknown <wasitpolmaliwan@gmail.com>
2023-06-09 00:57:39 +0700

    add

--- 0019.py
+++ 0019.py
@@ -1,9 +1,26 @@
+data = {"S":[],"B":[]}
 n = int(input())
-data = {}
-for i in range(n):
-    s,b = [int(a) for a in input().split()]
-    data[i] = [s,b]
+best = 1000000000
 
-def match_function(n):
+def diff(a,b):
+    return abs(a-b)
+
+def recusion(i,sourness,bitter):
+    global best
+    if i == n:
+        if sourness > 0 and diff(sourness,bitter) < best:
+            best = diff(sourness,bitter)
+    else:
+        recusion(i+1,sourness,bitter) 
+        recusion(i+1,sourness*data["S"][i],bitter+data["B"][i])
+    
+def main():
     for i in range(n):
-        
+        s, b = [int(x) for x in input().split()]
+        data['S'].append(s)
+        data['B'].append(b)
+    recusion(0,1,0)
+    print(best)
+    
+if __name__ == "__main__":
+    main()
